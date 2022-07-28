d5b3b0fd302e237b45d3716c8570b0959d4163fb
Cyber-z3ter <93003405+Cyber-z3ter@users.noreply.github.com>
2022-07-28 20:06:25 +0100

    Update ft_strlen.c

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -12,12 +12,14 @@
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *c)
+size_t	ft_strlen(const char *str)
 {
 	size_t	i;
 
 	i = 0;
-	while (c[i])
+	if (!str)
+		return(0);
+	while (str[i])
 		i++;
 	return (i);
 }
